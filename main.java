79e586207628ad6ef273a4aeea96068b19e41b85
amoore2829 <99160263+amoore2829@users.noreply.github.com>
2022-02-20 22:03:47 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,20 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
-/**
- *
- * @author thoma
- */
 public class main
 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
         int[][] validOne =  {{2,7,6}, {9,5,1}, {4,3,8}};
